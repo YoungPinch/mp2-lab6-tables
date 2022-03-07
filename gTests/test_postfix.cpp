b50176434ff6b87a24fc675ed56ee54a31c330ac
Лев Поляков <lev-polyackov@mail.ru>
2022-03-07 22:05:38 +0300

    add tests to postfix

--- gTests/test_postfix.cpp
+++ gTests/test_postfix.cpp
@@ -1,7 +1,124 @@
-#include "pch.h"
+﻿#include "pch.h"
 
-TEST(Postfix, Test1) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TPostfix, can_create_postfix)
+{
+	ADD_FAILURE();
 }
 
+TEST(TPostfix, can_create_postfix_with_correct_brackets)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, throws_when_create_postfix_with_incorrect_placement_of_brackets)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, throws_when_create_postfix_with_too_much_closing_brackets)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, throws_when_create_postfix_with_too_much_opening_brackets)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_create_postfix_with_correct_names_of_variables)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, throws_when_create_postfix_with_incorrect_name_of_variable)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_infix)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_string_infix)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_postfix)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_postfix_with_operations_which_have_different_priorities)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_string_postfix)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_string_postfix_with_operations_which_have_different_priorities)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_postfix_with_expressions_with_brackets)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_get_string_postfix_with_expressions_with_brackets)
+{
+	ADD_FAILURE();
+}
+
+// +
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_plus)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_plus_and_variables)
+{
+	ADD_FAILURE();
+}
+
+// -
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_minus)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_minus_and_variables)
+{
+	ADD_FAILURE();
+}
+
+// *
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_multiplication)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_multiplication_and_variables)
+{
+	ADD_FAILURE();
+}
+
+// /
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_division)
+{
+	ADD_FAILURE();
+}
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_division_and_variables)
+{
+	ADD_FAILURE();
+}
