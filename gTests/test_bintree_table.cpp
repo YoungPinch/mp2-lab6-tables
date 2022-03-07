f7fdfe923bc2d6a5e41b6cc976cc90927534311c
unknown <dreamerandrew17@gmail.com>
2022-03-07 21:28:31 +0300

    added tests

---
+++ gTests/test_bintree_table.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+TEST(bintree, test1) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
---
+++ gTests/test_polinom.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+TEST(Polinom, Test2) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
