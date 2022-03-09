f542167ca6f21c029d3b56340e96017c0130ab37
Лев Поляков <lev-polyackov@mail.ru>
2022-03-09 09:00:47 +0300

    added TableManager

--- mp2-lab6-tables/tables.hpp
+++ mp2-lab6-tables/tables.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string.h>
 #include "polinom.hpp"
+#define COUNTTABLES 6
 
 class PolinomObj {
 private:
@@ -21,91 +22,88 @@ public:
   }
 };
 
+class TableManager {
+private:
+  TTable* activeTable;
+  TTable* tables[COUNTTABLES];
+
+  enum TableKind
+  {
+    SORTARR = 0,
+    UNSORTARR,
+    BINTREE,
+    UNSORTLIST,
+    CHAINHASH,
+    OPENHASH
+  };
+
+  void Insert(PolinomObj* obj) {
+    for (int i = 0; i < COUNTTABLES; i++)
+      tables[i]->Insert(obj);
+  }
+
+public:
+  TableManager() {
+    tables[SORTARR] = new SortArrayTable();
+    tables[UNSORTARR] = new UnsortArrayTable();
+    tables[BINTREE] = new BinaryTable();
+    tables[UNSORTLIST] = new UnsortListTable();
+    tables[CHAINHASH] = new ChainHashTable();
+    tables[OPENHASH] = new OpenHashTable();
+  }
+
+  inline void SetActiveTable(int ind) { activeTable = tables[ind]; }
+
+  void Insert(std::string _name, Polinom* pol) { Insert(PolinomObj::Create(_name, pol)); }
+
+  void Insert(std::string _name, std::string _strPol) { Insert(PolinomObj::Create(_name, _strPol)); }
+
+  void Delete(std::string _name);
+
+  void Print() { std::cout << activeTable; }
+
+};
+
 class TTable {
-private: 
-  void ShowTableHeader();
 protected:
   virtual PolinomObj* FindObj(std::string name);
 public:
   virtual void Delete(std::string _name);
   virtual const Polinom& Find(std::string name);
-  virtual void Insert(std::string _name, std::string _strPol);
-  virtual void Insert(std::string _name, Polinom* pol);
-  virtual void Show();
+  virtual void Insert(PolinomObj* obj);
+  virtual friend std::ostream& operator<<(std::ostream& os, const TTable* table);
   virtual ~TTable() = 0;
 };
 
 class SortArrayTable final : public TTable {
 private:
-  virtual PolinomObj* FindObj(std::string name);
-
-  PolinomObj* arr[];
-
+  PolinomObj** arr;
 public:
-  void Show();
-  void Delete(std::string _name);
-  const Polinom& Find(std::string name);
-  void Insert(std::string _name, std::string _strPol);
-  void Insert(std::string _name, Polinom* pol);
 };
 
 class UnsortArrayTable final : public TTable {
 private:
-  virtual PolinomObj* FindObj(std::string name);
-
-  PolinomObj* arr[];
-
+  PolinomObj** arr;
 public:
-  void Show();
-  void Delete(std::string _name);
-  const Polinom& Find(std::string name);
-  void Insert(std::string _name, std::string _strPol);
-  void Insert(std::string _name, Polinom* pol);
-};
-
-class UnsortListTable final : public TTable {
-private:
-  virtual PolinomObj* FindObj(std::string name);
-
-  TList<PolinomObj*> list;
-
-public:
-  void Show();
-  void Delete(std::string _name);
-  const Polinom& Find(std::string name);
-  void Insert(std::string _name, std::string _strPol);
-  void Insert(std::string _name, Polinom* pol);
 };
 
 class BinaryTable final : public TTable {
 private:
-  virtual PolinomObj* FindObj(std::string name);
 public:
-  void Show();
-  void Delete(std::string _name);
-  const Polinom& Find(std::string name);
-  void Insert(std::string _name, std::string _strPol);
-  void Insert(std::string _name, Polinom* pol);
+};
+
+class UnsortListTable final : public TTable {
+private:
+  TList<PolinomObj*> list;
+public:
 };
 
 class ChainHashTable final : public TTable {
 private:
-  virtual PolinomObj* FindObj(std::string name);
 public:
-  void Show();
-  void Delete(std::string _name);
-  const Polinom& Find(std::string name);
-  void Insert(std::string _name, std::string _strPol);
-  void Insert(std::string _name, Polinom* pol);
 };
 
 class OpenHashTable final : public TTable {
 private:
-  virtual PolinomObj* FindObj(std::string name);
 public:
-  void Show();
-  void Delete(std::string _name);
-  const Polinom& Find(std::string name);
-  void Insert(std::string _name, std::string _strPol);
-  void Insert(std::string _name, Polinom* pol);
 };
