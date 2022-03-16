d4f54cbc1dba49a7c40a49c5bbebce49beef2920
SergeyZagrebin <sergey.127.0.0.1@ya.ru>
2022-03-16 12:20:13 +0300

    S: shared_ptr + my fixes

---
+++ mp2-lab6-tables/UnsortListTable.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <tables.hpp>
+
+class UnsortListTable final : public TTable {
+private:
+    TList<std::shared_ptr<PolinomObj>> list;
+public:
+    void Insert(std::shared_ptr<PolinomObj> obj) {
+        list.InsertFirst(obj);
+    }
+    const Polinom& Find(std::string name) {
+        for(auto it = list.cbegin(); it != list.cend(); ++it)
+            if((*it)->name == name)
+                return (*it)->pol;
+        throw -1;
+    }
+    void Delete(std::string _name){
+        for(auto it = list.cbegin(); it != list.cend(); ++it)
+            if((*it)->name == _name)
+                list.Delete(it);
+    }
+};
