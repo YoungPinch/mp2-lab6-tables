dd6b71e7ada2beb04d4feae50e33c67c6f358103
SergeyZagrebin <sergey.127.0.0.1@ya.ru>
2022-03-29 15:35:59 +0300

    Lio's Change UnsortArray

--- tables/UnsortArrayTable.h
+++ tables/UnsortArrayTable.h
@@ -6,22 +6,57 @@ template <class TKey, class TData>
 class UnsortArrayTable final : public TTable<class TKey, class TData> {
 private:
   std::vector<TData> data;
-  int size;
+  int maxInd;
 
 public:
-  UnsortArrayTable(int sz) : size(sz), data(size) {}
+  UnsortArrayTable(int sz) : data(sz), maxInd(0) {}
 
-  void Insert(TData data) {};
+  void Insert(TData _data) {
+    if (data.size() == maxInd)
+      throw 1;
+    for (int i = 0; i < data.size(); i++) {
+      if (data[i] == TData()) {
+        data[i] = _data;
+        if (i == maxInd)
+          maxInd++;
+        break;
+      }
+    }
+  }
 
-  TData& Find(TKey) {
+  TData& Find(TKey name) {
     for (int i = 0; i < size; i++)
-      if (data[i].get() != nullptr && data[i].get()->getName() == name)
+      if (data[i] != TData() && data[i] == name)
         return data[i];
     return TData();  
   }
 
-  void Delete(TKey name) {};
+  void Delete(TKey name) {
+    int i = 0;
+    for (; i < maxInd; i++) {
+      if (data[i] != TData() && data[i] == name) {
+        data[i] = TData();
+        break;
+      }
+    }
 
-  void Print() {};
+    if (i == maxInd - 1) {
+      for (; i > 0; i--) {
+        if (data[i] == TData())
+          maxInd--;
+        else
+          break;
+      }
+    }
+  }
+
+  void Print() {
+    for (int i = 0; i < maxInd; i++) {
+      if (data[i] != TData())
+        std::cout << data[i];
+      else
+        std::cout << '\n';
+    }
+  }
 };
 
