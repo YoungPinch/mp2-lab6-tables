62abbb3613750e4515139a5c1e87f7411571b91a
YoungPinch <dreamerandrew17@gmail.com>
2022-04-06 21:06:45 +0300

    friends with sad

--- tests/test_postfix.cpp
+++ tests/test_postfix.cpp
@@ -1,124 +1,229 @@
-﻿#include "gtest/gtest.h"
+﻿#pragma once
+#include "gtest/gtest.h"
+#include "TPostfix.h"
+#include "TPostfix.cpp"
 
+struct JopaSlona
+{
+  TableManager tableneger;
+  JopaSlona(int size):tableneger(size)
+  {
+	tableneger.Insert("sad", "-3x2+y3");
+	tableneger.Insert("bad", "xyz-1");
+	tableneger.Insert("ded", "z3+x2y");
+	tableneger.Insert("Inside", "x7-z7");
+  }
+};
 TEST(TPostfix, can_create_postfix)
 {
-  ADD_FAILURE();
+  ASSERT_NO_THROW(TPostfix p());
+}
+
+TEST(TPostfix, can_create_postfix_with_argument)
+{
+  string polStr = "xyz+3x2y2";
+
+  ASSERT_NO_THROW(TPostfix p(polStr));
 }
 
 TEST(TPostfix, can_create_postfix_with_correct_brackets)
 {
-  ADD_FAILURE();
-}
+  string polStr = "(xyz+3x2y2) + (x2-z)";
 
-TEST(TPostfix, throws_when_create_postfix_with_incorrect_placement_of_brackets)
-{
-  ADD_FAILURE();
+  ASSERT_NO_THROW(TPostfix p(polStr));
 }
 
 TEST(TPostfix, throws_when_create_postfix_with_too_much_closing_brackets)
 {
-  ADD_FAILURE();
+  string polStr = "(xyz+3x2y2)) + (x2-z)";
+
+  ASSERT_ANY_THROW(TPostfix p(polStr));
 }
 
 TEST(TPostfix, throws_when_create_postfix_with_too_much_opening_brackets)
 {
-  ADD_FAILURE();
+  string polStr = "((xyz+3x2y2) + (x2-z)";
+
+  ASSERT_ANY_THROW(TPostfix p(polStr));
 }
 
 TEST(TPostfix, can_create_postfix_with_correct_names_of_variables)
 {
-  ADD_FAILURE();
+  string polStr = "sad + bad - bold + 3x2";
+
+  ASSERT_NO_THROW(TPostfix p(polStr));
 }
 
 TEST(TPostfix, throws_when_create_postfix_with_incorrect_name_of_variable)
 {
-  ADD_FAILURE();
+  string polStr = "3sad+3x2";
+
+  ASSERT_ANY_THROW(TPostfix p(polStr));
 }
 
 TEST(TPostfix, can_get_infix)
 {
-  ADD_FAILURE();
+  string polStr = "xyz+3x2y2";
+
+  ASSERT_NO_THROW(TPostfix(polStr).GetInfix());
 }
 
 TEST(TPostfix, can_get_string_infix)
 {
-  ADD_FAILURE();
+  string polStr = "3x2y2 + z2 ";
+  string result = TPostfix(polStr).GetStringInfix();
+  string a;
+  EXPECT_EQ(polStr, TPostfix(polStr).GetStringInfix());
 }
 
 TEST(TPostfix, can_get_postfix)
 {
-  ADD_FAILURE();
-}
+  string polStr = "3x2y2 + z2";
 
-TEST(TPostfix, can_get_postfix_with_operations_which_have_different_priorities)
-{
-  ADD_FAILURE();
+  EXPECT_NO_THROW(TPostfix(polStr).GetPostfix());
 }
 
 TEST(TPostfix, can_get_string_postfix)
 {
-  ADD_FAILURE();
+  string polStr = "3x2y2 + z2";
+  string postfix = "3x2y2 z2 + ";
+
+  EXPECT_EQ(postfix, TPostfix(polStr).GetStringPostfix());
+}
+
+TEST(TPostfix, can_get_postfix_with_operations_which_have_different_priorities)
+{
+  //string polStr = "(3x2+y2) * 3 - d(2x3 + xyz)/dx";
+  string polStr = "3x2 + y2 * 2";
+
+  EXPECT_NO_THROW(TPostfix(polStr).GetPostfix());
 }
 
 TEST(TPostfix, can_get_string_postfix_with_operations_which_have_different_priorities)
 {
-  ADD_FAILURE();
+  string polStr = "3x2 + y2 * 2";
+  string postfix = "3x2 y2 2 * + ";
+
+  EXPECT_EQ(postfix, TPostfix(polStr).GetStringPostfix());
 }
 
 TEST(TPostfix, can_get_postfix_with_expressions_with_brackets)
 {
-  ADD_FAILURE();
+  string polStr = "(3x2 + y2) * 2";
+
+  EXPECT_NO_THROW(TPostfix(polStr).GetPostfix());
 }
 
 TEST(TPostfix, can_get_string_postfix_with_expressions_with_brackets)
 {
-  ADD_FAILURE();
+  string polStr = "(3x2 + y2) * 2";
+  string postfix = "3x2 y2 + 2 * ";
+
+  EXPECT_NO_THROW(TPostfix(polStr).GetStringPostfix());
 }
 
 // +
 
 TEST(TPostfix, can_calculate_arithmetic_expression_with_plus)
 {
-  ADD_FAILURE();
+  string polStr = "3xyz + 7xyz + x3";
+  Polinom result("x3+10xyz");
+
+  EXPECT_EQ(result, TPostfix(polStr).Calculate());
+}
+
+TEST(TPostfix, can_be_friends_with_sad)
+{
+  string polStr = "(((((sad)))))";
+  Polinom result("-3x2+y3");
+
+  JopaSlona js(10);
+
+  EXPECT_EQ(result, TPostfix(polStr, &js.tableneger).Calculate());
 }
 
 TEST(TPostfix, can_calculate_arithmetic_expression_with_plus_and_variables)
 {
-  ADD_FAILURE();
+  string polStr = "sad + bad + 3x2 + 7";
+  Polinom result("xyz+y3+6");
+
+  JopaSlona js(10);
+  EXPECT_EQ(result, TPostfix(polStr, &js.tableneger).Calculate());
 }
 
 // -
 
 TEST(TPostfix, can_calculate_arithmetic_expression_with_minus)
 {
-  ADD_FAILURE();
+  string polStr = "3x2 - 3x2 + xyz";
+  Polinom result("xyz");
+
+  EXPECT_EQ(result, TPostfix(polStr).Calculate());
 }
 
 TEST(TPostfix, can_calculate_arithmetic_expression_with_minus_and_variables)
 {
-  ADD_FAILURE();
+  string polStr = "sad - bad - 3x2 + 7";
+  Polinom result("-6x2-xyz+y3+8");
+
+  JopaSlona js(10);
+  EXPECT_EQ(result, TPostfix(polStr, &js.tableneger).Calculate());
 }
 
 // *
 
 TEST(TPostfix, can_calculate_arithmetic_expression_with_multiplication)
 {
-  ADD_FAILURE();
+  string polStr = "3x2 * 3x2 * xyz * 2";
+  Polinom result("18x5yz");
+
+  EXPECT_EQ(result, TPostfix(polStr).Calculate());
 }
 
 TEST(TPostfix, can_calculate_arithmetic_expression_with_multiplication_and_variables)
 {
-  ADD_FAILURE();
+  string polStr = "sad * bad";
+  Polinom result("-3x3yz+3x2+xy4z-y3");
+
+  JopaSlona js(10);
+  EXPECT_EQ(result, TPostfix(polStr, &js.tableneger).Calculate());
 }
 
-// /
+// d
 
-TEST(TPostfix, can_calculate_arithmetic_expression_with_division)
+TEST(TPostfix, can_calculate_arithmetic_expression_with_differentional)
 {
-  ADD_FAILURE();
+  string polStr = "d(3xy2z - z3 + y)/dz";
+  Polinom result("3xy2-3z2");
+
+  EXPECT_EQ(result, TPostfix(polStr).Calculate());
 }
 
-TEST(TPostfix, can_calculate_arithmetic_expression_with_division_and_variables)
+TEST(TPostfix, can_calculate_arithmetic_expression_with_differentional_and_variables)
 {
-  ADD_FAILURE();
+  string polStr = "d(sad)/dx + d(Inside)/dz";
+  Polinom result("-6x-7z6");
+
+  JopaSlona js(10);
+  EXPECT_EQ(result, TPostfix(polStr, &js.tableneger).Calculate());
 }
+
+//I
+TEST(TPostfix, can_calculate_arithmetic_expression_with_integral)
+{
+  string polStr = "I(x7+z2)dx + I(x2)dz";
+  Polinom result("0.125x8+x2z+xz2");
+
+  EXPECT_EQ(result, TPostfix(polStr).Calculate());
+}
+
+TEST(TPostfix, can_calculate_arithmetic_expression_with_integral_and_variables)
+{
+  string polStr = "I(sad)dx + I(Inside)/dz";
+  Polinom result("-1x3+x7z+xy3-0.125z8");
+
+  JopaSlona js(10);
+  EXPECT_EQ(result, TPostfix(polStr, &js.tableneger).Calculate());
+}
+
+
